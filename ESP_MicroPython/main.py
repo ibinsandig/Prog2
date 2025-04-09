17b89854960ebf53dfc2d0cb3ea9bb362363520f
ibinsandig <lbraun0203@gmail.com>
2025-04-09 13:30:26 +0200

    test feuchtigkeits

--- ESP_MicroPython/main.py
+++ ESP_MicroPython/main.py
@@ -4,13 +4,22 @@ def led_blink():
         time.sleep(0.5) # type : ignore
         bootled.off() # type : ignore
         time.sleep(0.5) # type : ignore
+        
+def transistor_test():
+    for _ in range(3):
+        transistor.on() # type : ignore
+        time.sleep(10) # type : ignore
+        transistor.off() # type : ignore
+        time.sleep(3) # type : ignore
 
-led_blink()
+def test_feuchtigkeit(anzahl):
+    while anzahl > 10:
+        Serial.print("Digital: ", digital_value) # type : ignore
+        Serial.print("Analog: ", analog_value) # type : ignore
+        time.sleep(1)
+        anzahl += 1
 
-for _ in range(3):
-    transistor.on() # type : ignore
-    time.sleep(10) # type : ignore
-    transistor.off() # type : ignore
-    time.sleep(3) # type : ignore
 
-led_blink()
+
+test_feuchtigkeit(10)
+
