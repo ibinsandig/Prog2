196fb3ea8e27cfa2795f8175d8b1a53d56b90deb
ibinsandig <lbraun0203@gmail.com>
2025-04-09 13:08:38 +0200

    environment und pip update

--- ESP_MicroPython/boot.py
+++ ESP_MicroPython/boot.py
@@ -1,15 +1,27 @@
-from machine import Pin # type: ignore
+import machine as mech
 import time
 
-transistor = Pin(5, Pin.OUT)
+# Pin zur Aktivierung der Pumpe durch TRansistor
+transistor = mech.Pin(5, mech.Pin.OUT) # type : ignore
 transistor.off()
 
-led = Pin(2, Pin.OUT)
-led.off()
+# Pin für den digitalen EIngang des Feuchtigkeitssensors
+feuchtigkeitDigital = mech.Pin(4, mech.Pin.IN) # type : ignore
+digital_value = feuchtigkeitDigital.value()
 
-# Blinksignal zur Anzeige des Bootens
+# Pin für den analogen Eingang des Feuchtigkeitssensors
+feuchtigkeitAnalog = mech.ADC(0)
+analog_value = feuchtigkeitAnalog.read()
+
+# Boot LED
+bootled = mech.Pin(2, mech.Pin.OUT) # type : ignore
+bootled.off()
+
+# Blinksignal beendigung des Bootvorgangs
 for _ in range(10):
-    led.on()
+    bootled.on()
     time.sleep(0.2)
-    led.off()
+    bootled.off()
     time.sleep(0.2)
+
+bootled.on()
--- ESP_MicroPython/main.py
+++ ESP_MicroPython/main.py
@@ -1,16 +1,16 @@
 def led_blink():
     for _ in range(3):
-        led.on()
-        time.sleep(0.5)
-        led.off()
-        time.sleep(0.5)
+        bootled.on()    # type : ignore
+        time.sleep(0.5) # type : ignore
+        bootled.off() # type : ignore
+        time.sleep(0.5) # type : ignore
 
 led_blink()
 
 for _ in range(3):
-    transistor.on()
-    time.sleep(10)
-    transistor.off()
-    time.sleep(3)
+    transistor.on() # type : ignore
+    time.sleep(10) # type : ignore
+    transistor.off() # type : ignore
+    time.sleep(3) # type : ignore
 
 led_blink()
