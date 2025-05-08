36112930d2ea862ab8e023f125a0547db4ab60aa
ibinsandig <lbraun0203@gmail.com>
2025-05-08 12:59:47 +0200

    Add initial HTML templates and JavaScript for navigation
    - Created `script.js` to handle page navigation with functions for main page, control panel, and info page.
    - Added `control.html` template with a button to return to the main page.
    - Developed `index.html` template with welcome message and buttons for navigation to info and control panel.
    - Implemented `info.html` template with a button to return to the main page.
    - Included links to the JavaScript file and CSS styles in all templates.

---
+++ py_server/src/static/js/script.js
@@ -0,0 +1,11 @@
+console.log("Hello from script.js");
+document.getElementById("imp").textContent = "Hello from script.js";
+function ZumBedienpanel(){
+    window.location.href = "/control";
+}
+function ZurMainpage(){
+    window.location.href = "/";
+}
+function ZurInfo(){
+    window.location.href = "/info";
+}
