433c8bc5af6e2e8be4928c6838d74bc181fd21e8
ibinsandig <lbraun0203@gmail.com>
2025-05-13 12:23:10 +0200

    asas

---
+++ ESP_MicroPython/test mqtt/boot.py
@@ -0,0 +1,22 @@
+import machine # type: ignore
+import time
+import network # type: ignore
+import uqmtt
+
+# WLAN-Station-Modus aktivieren
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+
+# Mit WLAN verbinden
+ssid = "WehLan"
+password = "1234wlan5678"
+wlan.connect(ssid, password)
+
+# Warten, bis die Verbindung hergestellt ist
+while not wlan.isconnected():
+    pass
+    print(".")
+    time.sleep(0.5)
+
+print("Verbunden! IP-Adresse:", wlan.ifconfig()[0])
+
---
+++ ESP_MicroPython/test mqtt/main.py
@@ -0,0 +1,9 @@
+from machine import Pin
+import time
+
+led_green = Pin(12, Pin.OUT)
+
+led_green.on()
+time.sleep(5)
+led_green.off()
+
