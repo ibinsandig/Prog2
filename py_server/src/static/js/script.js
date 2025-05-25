69baae87abf36b7f55e9d3724780786f952a603f
Linus Braun <lbraun0203@gmail.com>
2025-05-25 16:13:41 +0000

    update kommunikation

--- py_server/src/static/js/script.js
+++ py_server/src/static/js/script.js
@@ -10,6 +10,25 @@ function ZurInfo(){
     window.location.href = "/info";
 }
 
+function controlPump(turnOn) {
+    const action = turnOn ? 'on' : 'off';
+
+    fetch('/api/pump-control', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ action: action })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Pumpenstatus aktualisiert:", data);
+    })
+    .catch(error => {
+        console.error("Fehler beim Steuern der Pumpe:", error);
+    });
+}
+
 function fetchLatestData() {
     fetch('/api/latest-data')
         .then(response => response.json())
@@ -36,4 +55,14 @@ function fetchLatestData() {
         });
 }
 
+function refreshPlot() {
+    const plotImg = document.getElementById("moisture-plot");
+    const timestamp = new Date().getTime();  // Cache-Busting
+    plotImg.src = `/moisture-plot?t=${timestamp}`;
+}
+
+// 3,1 Sekunden Aktualisierung
+setInterval(refreshPlot, 3100);
+
+// 3 Sekunden Aktualisierung
 setInterval(fetchLatestData, 3000);
